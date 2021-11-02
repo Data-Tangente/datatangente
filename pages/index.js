ba89881b08ece02a374697691fffadb6b2cdfd36
Edward Gonzalez <edward.gonzalez@minerd.gob.do>
2021-11-02 18:08:41 -0400

    Revert "initial commit web"
    This reverts commit 14628b496139dc8af5d03c4b9fd1a2944b620b01.

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import Slideshow from './../components/Slideshow';
-import Division from  './../components/Division';
-import BigDivision from  './../components/BigDivision';
-import WorkedDivision from  './../components/WorkedDivision';
-import PostCard from  './../components/PostCard';
-import SubsDivision from  './../components/SubscribeDivision';
-// import Projects from  './../components/Projects';
-import imagesLoaded from 'imagesloaded';
-import marked from 'marked'
-import createDOMPurify from 'dompurify'
-import { JSDOM } from 'jsdom'
+const index = () => (
+	<di>
+		<h1>DataTangente</h1>
+		<p> Home Page</p>
+	</di>
+);
 
-export default function Home({posts}) {
-	return(
-		<div className="page-wrap">
-            <Slideshow />
-            <Division 
-                boldText="SOLUCIONES"
-                normalText="ACORDE A TUS NECESIDADES"
-                bgColor="#f05638"
-                textColor="#fff"
-            />
-            <BigDivision />
-            <Division 
-                boldText="HAN CONFIADO"
-                normalText="EN NUESTRO TRABAJO"
-                // bgColor="#fff"
-                // textColor="#dc5136"
-                bgColor="#fff"
-                textColor="#f05638"
-            />
-            <WorkedDivision 
-                bgColor="#f05638"
-            />
-            <Division 
-                boldText="PUBLICACIONES"
-                normalText="RECIENTES"
-                bgColor="#fff"
-                textColor="#dc5136"
-            />
-            <PostCard 
-                posts={posts}
-            />
-            <SubsDivision />
-        </div>
-	)
-}
-
-export async function getStaticProps() {
-
-    const allPosts = await fetch(`http://localhost:1337/posts`);
-    const allPostsData = await allPosts.json();
-    const host = process.env.HOST;
-
-    const window = new JSDOM('').window;
-    const DOMPurify = createDOMPurify(window);
-
-    allPostsData.forEach(item => {
-        item.content = DOMPurify.sanitize(marked(item.content));
-    })
-
-	return {
-        props: {posts: allPostsData.splice(0, 3) }
-    }
-}
+export default index;
