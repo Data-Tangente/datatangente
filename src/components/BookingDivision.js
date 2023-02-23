0c2128b85ec7aef4b72e5a67a757c4422a2b078f
gabycode <gabriellarosario72@gmail.com>
2023-02-23 16:15:58 -0400

    add & fix: components design

--- src/components/BookingDivision.js
+++ src/components/BookingDivision.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next';
-import { Grid } from '@material-ui/core'
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Grid } from "@material-ui/core";
 
 function BookingDivision() {
-    const { t } = useTranslation();
-    return (
-        <div className="booking-container max-width--">
-            <h1 className="booking-title">
-                <span className="booking_bold">{t('home.booking.book')} </span>
-                {t('home.booking.appointment')}
-            </h1>
-            <a
-                target="_blank"
-                href='https://jeandeleon-datatangente.zohobookings.com/#/customer/datatangente'
-                className="booking-button"
-            >
-                {t('home.booking.book-appt')}
-            </a>
-        </div>
-    )
+  const { t } = useTranslation();
+  return (
+    <div className="booking-container">
+      <h1 className="booking-title">
+        <span className="booking_bold">{t("home.booking.book")} </span>
+        {t("home.booking.appointment")}
+      </h1>
+      <a
+        target="_blank"
+        href="https://jeandeleon-datatangente.zohobookings.com/#/customer/datatangente"
+        className="booking-button">
+        {t("home.booking.book-appt")}
+      </a>
+    </div>
+  );
 }
 
 export default BookingDivision;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,59 +1,55 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 // import React, { useEffect } from 'react';
-import Slideshow from './../components/Slideshow';
-import Division from './../components/Division';
-import BigDivision from './../components/BigDivision';
-import WorkedDivision from './../components/WorkedDivision';
-import PostCard from './../components/PostCard';
-import SubsDivision from './../components/SubscribeDivision';
-import BookingDivision from './../components/BookingDivision';
-import { useTranslation } from 'next-i18next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { getPostsData } from '../functions/generalMethods';
+import Slideshow from "./../components/Slideshow";
+import Division from "./../components/Division";
+import BigDivision from "./../components/BigDivision";
+import WorkedDivision from "./../components/WorkedDivision";
+import PostCard from "./../components/PostCard";
+import SubsDivision from "./../components/SubscribeDivision";
+import BookingDivision from "./../components/BookingDivision";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { getPostsData } from "../functions/generalMethods";
 
 export default function Home({ posts }) {
-    const { t } = useTranslation();
-    return (
-        <div className="page-wrap">
-            <Slideshow />
-            <Division
-                boldText={t('home.divisions.solutions')}
-                normalText={t('home.divisions.needs')}
-                bgColor="#f05638"
-                textColor="#fff"
-            />
-            <BigDivision />
-            <Division
-                boldText={t('home.divisions.trusted')}
-                normalText={t('home.divisions.work')}
-                bgColor="#fff"
-                textColor="#f05638"
-            />
-            <WorkedDivision
-                bgColor="#f05638"
-            />
-            <Division
-                boldText={t('home.divisions.posts')}
-                // normalText={t('home.divisions.recent')}
-                bgColor="#fff"
-                textColor="#f05638"
-            />
-            <PostCard
-                posts={posts}
-            />
-            <SubsDivision />
-            <BookingDivision />
-        </div>
-    )
+  const { t } = useTranslation();
+  return (
+    <div className="page-wrap">
+      <Slideshow />
+      <BookingDivision />
+      {/* <Division
+        boldText={t("home.divisions.solutions")}
+        normalText={t("home.divisions.needs")}
+        bgColor="#f05638"
+        textColor="#fff"
+      /> */}
+      <BigDivision />
+      {/* <Division
+        boldText={t("home.divisions.trusted")}
+        normalText={t("home.divisions.work")}
+        bgColor="#f05638"
+        textColor="#fff"
+      /> */}
+      <WorkedDivision bgColor="#f05638" />
+      <Division
+        boldText={t("home.divisions.posts")}
+        // normalText={t('home.divisions.recent')}
+        bgColor="#fff"
+        textColor="#f05638"
+      />
+      <PostCard posts={posts} />
+      <SubsDivision />
+    </div>
+  );
 }
 
 export async function getStaticProps({ locale }) {
-    const data = await getPostsData(locale);
-    return {
-        props: {
-            ...(await serverSideTranslations(locale, ['common'])),
-            ...data,
-            // ...countryLanguages,
-        }
-    };
+  const data = await getPostsData(locale);
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      ...data,
+      // ...countryLanguages,
+    },
+  };
 }
