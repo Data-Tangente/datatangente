abc68ea72361cbe76adb1ef32d06365c464a2df4
gabycode <gabriellarosario72@gmail.com>
2023-02-28 14:37:01 -0400

    fix: subscribe component ui

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,13 +3,14 @@ import SubsDivision from "../components/SubscribeDivision";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import BookingDivision from "../components/BookingDivision";
 import ContactForm from "../components/ContactForm";
+import NewsletterSubscribeContact from "../components/NewsletterSubscribeContact";
 
 function Contact() {
   return (
     <>
       <BookingDivision />
       <ContactForm />
-      <SubsDivision />
+      <NewsletterSubscribeContact />
     </>
   );
 }
